b12a0db3838ddfb8d37c6780e3c2a5d598a5593b
MikyPopescu <mihaellap42@gmail.com>
2020-07-04 16:09:33 +0300

    finish

--- app.js
+++ app.js
@@ -44,7 +44,9 @@ mongoose.set("useCreateIndex",true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId:String,
+    secret:String
 });
 
 userSchema.plugin(passportLocalMongoose); //hash+salt passwords + save users in db
@@ -85,9 +87,11 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
+
 app.get("/auth/google",function(req,res){
     passport.authenticate("google",{ scope: ["profile"] });
 });
+
 
 app.get("/auth/google/secrets",
     passport.authenticate("google",{ failureRedirect: "/login" }),
@@ -105,12 +109,53 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets",function(req,res){
+    // if(req.isAuthenticated()){
+    //     res.render("secrets");
+    // }
+    // else{
+    //     res.redirect("/login");
+    // }
+
+    User.find({"secret": {$ne:null}},function(err,foundUsers){
+        if(err){
+            console.log(err);
+            
+        }
+        else{
+            if(foundUsers){
+                res.render("secrets",{usersWithSecrets:foundUsers});
+            }
+        }
+    });
+});
+
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }
     else{
         res.redirect("/login");
     }
+});
+
+app.post("/submit",function(req,res){
+const submittedSecret = req.body.secret;
+
+
+User.findById(req.user.id,function(err,foundUser){
+    if(err){
+        console.log(err);
+    
+    }
+    else{
+        if(foundUser){
+            foundUser.secret=submittedSecret;
+            foundUser.save(function(){
+                res.redirect("/secrets")
+        });
+        }
+    }
+});
 });
 
 app.get("/logout",function(req,res){
